3efca56959e681eb23808fd622c92a2c685863f4
lukebooysen <lukeedwardbooysen@gmail.com>
2019-02-26 09:47:44 +0200

    Test post /todos route

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.post('/todos', (req, res) => {
     todo.save().then((doc) => {
         res.send(doc);
     }, (e) => {
-        res.send(e);
+        res.status(400).send(e);
     });
 
 });
@@ -25,3 +25,5 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
+
+module.exports = {app};
