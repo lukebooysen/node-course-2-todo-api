b4ee3f16ec291b2b6893579adf2af20842d02319
lukebooysen <lukeedwardbooysen@gmail.com>
2019-02-28 09:08:30 +0200

    Add test cases for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const request = require('supertest');
 const expect = require('expect');
+const {ObjectID} = require('mongodb');
 
 const{app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+    _id : new ObjectID(),
     text: 'First test todo'
 }, {
+    _id : new ObjectID(),
     text: 'Second test todo'
 }];
 
@@ -18,7 +21,7 @@ beforeEach((done) => {
 
 describe('POST /todos', () => {
     it('Should create a new todo', (done) => {
-        var text = 'Test todo text';
+        var text = 'First test todo';
 
         request(app)
         .post('/todos')
@@ -33,7 +36,7 @@ describe('POST /todos', () => {
             }
 
             Todo.find(text).then((todos) => {
-                expect(todos.length).toBe(1);
+                expect(todos.length).toBe(3);
                 expect(todos[0].text).toBe(text);
                 done();
             }).catch((e) => done(e));
@@ -68,3 +71,32 @@ describe('GET /todos', () => {
         .end(done);
     });
 })
+
+describe('GET /todos/:id', () => {
+    it('should return todo doc', (done) => {
+        request(app)
+        .get(`/todos/${todos[0]._id.toHexString()}`)
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo.text).toBe(todos[0].text);
+        })
+        .end(done);
+    });
+
+    it('Should return a 404 if todo not found', (done) => {
+        var hexId = new ObjectID().toHexString();
+        request(app)
+        .get(`/todos/${hexId}`)
+        .expect(404)
+        .end(done);
+    });
+
+    it('Should return a 404 for non-object ids', (done) => {
+        // /todos/123
+        request(app)
+        .get('/todos/123abc')
+        .expect(404)
+        .end(done);
+    });
+
+});
