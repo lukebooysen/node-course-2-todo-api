10b0188c93aa7dcb2c9cdbf738bf4d3ad87e98d7
lukebooysen <lukeedwardbooysen@gmail.com>
2019-03-08 09:33:50 +0200

    ADD tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,6 +4,7 @@ const {ObjectID} = require('mongodb');
 
 const{app} = require('./../server');
 const {Todo} = require('./../models/todo');
+const {User} = require('./../models/user');
 const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
 
 beforeEach(populateUsers);
@@ -259,8 +260,8 @@ describe('POST /users/login', () => {
                 if (err) {
                     return done(err);
                 }
-                User.findById(user[1]._id).then((user) => {
-                    expect(user.tokens[0]).toInclude({
+                User.findById(users[1]._id).then((user) => {
+                    expect(users.tokens[0]).toInclude({
                         access: 'auth',
                         token: res.headers['x-auth']
                     });
@@ -284,10 +285,30 @@ describe('POST /users/login', () => {
                 if (err) {
                     return done(err);
                 }
-                User.findById(user[1]._id).then((user) => {
-                    expect(user.tokens.length).toBe(0);
+                User.findById(users[1]._id).then((user) => {
+                    expect(users.tokens.length).toBe(0);
                     done();
                 }).catch((e) => done(e));
+            });
+    });
+});
+
+describe('DELETE /users/me/token', () => {
+    it('should remove auth token on logout', (done) => {
+
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                User.findById(users[0]._id).then((user) => {
+                    expect(users.tokens.length).toBe(0);
+                    done();
+                }).catch((e) => done());
             });
     });
 });
